f775583ac8a90b76c212024b4c0de17a03c6b231
Adolfo <fitomad@users.noreply.github.com>
2016-09-10 18:35:14 +0200

    Add files via upload

--- UmpaLumpas/utils/bunny_client.rb
+++ UmpaLumpas/utils/bunny_client.rb
@@ -13,6 +13,8 @@ class BunnyClient
 	attr :channel
 	# Nombre de la colección con la que trabajamos
 	attr_reader :q_name
+	# Propiedades de conexion
+	attr_reader :connection_hash
 	# Usuario 
 	attr_reader :q_user
 	attr_reader :q_password
@@ -28,10 +30,10 @@ class BunnyClient
 		@error_count = 0
 
 		@q_name = "messages"
-		@q_user = "TU_USUARIO"
-		@q_password = "TU_PASSWORD"
+		@q_user = "WEB.SERV.adolfo.vera@outlook.com"
+		@q_password = "15F8BF9E-1B8C-47A7-8587-25A9B459E534"
 
-		connection_hash = 
+		@connection_hash = 
 		{
 			:host => "amqp.emtmadrid.es",
 			:port => "5672",
@@ -40,27 +42,6 @@ class BunnyClient
 			:auth_mechanism => "PLAIN",
 			:automatically_recover => true
 		}
-
-		@bunny_conn = Bunny.new(connection_hash)
-		@bunny_conn.start()
-
-		@channel = @bunny_conn.create_channel()
-	end
-
-	#
-	# ¿Todo listo para trabajar con Rabbit 
-	# y la cola especificada
-	#
-	def ok?
-		return @bunny_conn.queue_exists?(@q_name) 
-	end
-
-	#
-	# Cerramos la conexión
-	#
-	def close
-		@channel.close()
-		@bunny_conn.close()
 	end
 
 	#
@@ -74,6 +55,11 @@ class BunnyClient
 	# Publicamos el mensaje en Rabbit
 	#
 	def publish_datagram(datagram, type)
+		@bunny_conn = Bunny.new(@connection_hash)
+		@bunny_conn.start()
+
+		@channel = @bunny_conn.create_channel()
+
 		exchange = @channel.default_exchange()
 
 		exchange.on_return do | return_info, properties, content |
@@ -86,9 +72,15 @@ class BunnyClient
 			:mandatory => true, 
 			:routing_key => "messages", 
 			:type => type,
-			:content_type => "application/json")
+			:content_type => "application/json",
+			:user_id => @q_user)
 
 		@send_count += 1
+
+		#@channel.tx_commit()
+
+		@channel.close()
+		@bunny_conn.close()
 
 		print(status_message())
 		print("\r")
